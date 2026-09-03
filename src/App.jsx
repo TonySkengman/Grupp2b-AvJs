10bf2218a8bdadd7f4ba940c70cb8e039bdd8b10
Tony Le <tony.le00@hotmail.com>
2026-09-03 16:00:01 +0200

    re-add admin routing and related imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { CartProvider, useCart } from "./context/CartContext";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import Admin from "./pages/Admin";
 import ProductDetails from "./pages/ProductDetails";
 import Checkout from "./pages/Checkout";
 import OrderConfirmation from "./pages/OrderConfirmation";
@@ -14,6 +15,7 @@ function Nav() {
         <nav>
             <Link to="/">Hem</Link>
             <Link to="/cart">Varukorg ({totalItems})</Link>
+            <Link to="/admin">Butiksadmin</Link>
         </nav>
     );
 }
@@ -27,6 +29,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/cart" element={<Cart />} />
                     <Route path="/products/:id" element={<ProductDetails />} />
+                    <Route path="/admin" element={<Admin />} />
                     <Route path="/checkout" element={<Checkout />} />
                     <Route path="/order-confirmation" element={<OrderConfirmation />} />
                 </Routes>
