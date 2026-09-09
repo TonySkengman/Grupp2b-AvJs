ac013c9bdc91b2857ad28a9df0a8d556f47cb25e
Tony Le <tony.le00@hotmail.com>
2026-09-09 16:50:21 +0200

    add category filter component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,29 @@
-import ProductsList from "../components/ProductsLists";
 import { useEffect, useState } from "react";
+import CategoryFilter from "../components/CategoryFilter";
+import ProductsList from "../components/ProductsLists";
 
 export default function Home() {
     const [products, setProducts] = useState([]);
-
-    async function getProducts() {
-        const response = await fetch("/api/products");
-        const result = await response.json();
-
-        if (response.ok) {
-            setProducts(result);
-        } else {
-            console.log("Fetching products failed!")
-        }
-    }
+    const [filteredProducts, setFilteredProducts] = useState([]);
 
     useEffect(() => {
-        getProducts();
+        fetch("/api/products")
+            .then((response) => response.json())
+            .then((data) => {
+                setProducts(data);
+                setFilteredProducts(data);
+            });
     }, []);
 
     return (
-        <div>
-            <h1 className="title">Våra Produkter</h1>
-            <ProductsList products={products} />
-        </div>
-    )
+        <>
+
+            <CategoryFilter
+                products={products}
+                setFilteredProducts={setFilteredProducts}
+            />
+
+            <ProductsList products={filteredProducts} />
+        </>
+    );
 }
