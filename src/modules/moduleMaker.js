3e82626623a32de0299b4f22d47cd8617fc794b2
Pelmm <robin.palm@hotmail.com>
2026-09-13 23:31:14 +0200

    Move currency module entry point to index and wire moduleMaker

--- src/modules/moduleMaker.js
+++ src/modules/moduleMaker.js
@@ -1,7 +1,13 @@
 import CampaignModule from "./campaign/index.js";
+import CurrencyModule from "./currency-tax/index.js";
 
 const campaign = new CampaignModule();
+const currency = new CurrencyModule();
 export default {
+
   Campaign: campaign,
   CampaignDescriptor: CampaignModule.descriptor,
+  
+  Currency: currency,
+  CurrencyDescriptor: CurrencyModule.descriptor,
 }
